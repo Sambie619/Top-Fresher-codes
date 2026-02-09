0a663fbb370f58b08798e4877938a620016ad22a
Sabin Shah <168896473+Sambie619@users.noreply.github.com>
2026-02-09 16:12:20 +0530

    Add comments for recursive length function explanation
    Added comments explaining the recursive call expansion for the length function.

--- recursion/LenofString.java
+++ recursion/LenofString.java
@@ -19,3 +19,24 @@ public class LenofString {
         System.out.println(length(null));    // 0
     }
 }
+
+/*
+📞 Call Expansion (Going Down)
+length("hello")
+= 1 + length("ello")
+
+length("ello")
+= 1 + length("llo")
+
+length("llo")
+= 1 + length("lo")
+
+length("lo")
+= 1 + length("o")
+
+length("o")
+= 1 + length("")
+
+length("")
+= 0
+*/
